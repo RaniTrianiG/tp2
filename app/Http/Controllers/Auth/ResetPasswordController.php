04e3627a1b974fc5cf181a75ea5c345a1b8dbc1b
Farah Noriffat <nor.iffat32@gmail.com>
2022-09-29 21:00:36 +0700

    Reset Password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
 
 class ResetPasswordController extends Controller
 {
